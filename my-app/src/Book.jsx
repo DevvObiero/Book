5c369d364cbf558100a01f50f86a2fb55c6f3587
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-09-11 15:09:30 +0300

    imported rthe book component

---
+++ my-app/src/Book.jsx
@@ -0,0 +1,10 @@
+export default function Book() { 
+
+    return (
+        <div>
+            <h1>Book Component</h1>
+            <p>This is a simple book component.</p>
+        </div>
+    )
+
+}
