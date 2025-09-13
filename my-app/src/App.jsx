7e3eb5bbbfac423ebbd49c0ffb00630cf1c0000c
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-09-13 12:36:33 +0300

    working on the book project

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
 import Book from './Book.jsx'
-
+import Data from './data.js'
 
 
 function App() {
 
+
+  const Details = Data.map((item) => {
+    return
+    <Book
+      key={item.id}
+      title={item.title}
+      author={item.author}
+      price={item.price}
+    
+    />
+  })
   return (
     <>
    <Book/>
